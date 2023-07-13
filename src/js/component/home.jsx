79c34c42463bc11a921fa50443a6d967e55f01cb
Tcm68 <131905512+Tcm68@users.noreply.github.com>
2023-07-13 23:20:14 +0000

    fist commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,62 @@
-import React from "react";
+import React, { useState } from 'react';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const TodoList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const handleInputChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      addTask();
+    }
+  };
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, newTask]);
+      setNewTask('');
+    }
+  };
+
+  const deleteTask = (index) => {
+    setTasks(tasks.filter((_, i) => i !== index));
+  };
+
+  return (
+    <div className="todo-list-container">
+      <h1 className="todo-list-heading">Todo List</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Enter a task"
+          value={newTask}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyDown}
+          className="task-input"
+        />
+        <button onClick={addTask} className="add-button">
+          Add Task
+        </button>
+      </div>
+      {tasks.length === 0 ? (
+        <p className="empty-message">No tasks, add a task</p>
+      ) : (
+        <ul className="task-list">
+          {tasks.map((task, index) => (
+            <li key={index} className="task-item">
+              <span>{task}</span>
+              <button onClick={() => deleteTask(index)} className="delete-button">
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
 
-export default Home;
+export default TodoList;
